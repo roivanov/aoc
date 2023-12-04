c3ebdd7ac2c1322eda2c50783cb136897c3776d8
Roman Ivanov <me@roivanov.com>
2023-12-04 15:08:29 -0800

    day 2 part 2

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,19 +1,30 @@
 #!/bin/env python3
 
+RED='red'
+GREEN='green'
+BLUE='blue'
 count=0
+games_power=0
+
 with open('day2-data2') as f:
     for line in f.readlines():
         game, plays = line.strip('\n').split(':', 1)
         game_id = int(game.split(' ')[-1])
         valid_gate = True
+        max_cubes = {RED:0, GREEN:0, BLUE:0}
         for play in plays.split(';'):
             pairs = {k[1]:int(k[0]) for k in [x.strip().split() for x in play.split(',')]}
+            for each in [GREEN, BLUE, RED]:
+                max_cubes[each] = max(max_cubes[each], pairs.get(each, 0))
+
             # only 12 red cubes, 13 green cubes, and 14 blue cubes?
-            if pairs.get('red', 0) > 12 or pairs.get('green', 0) > 13 or pairs.get('blue', 0) > 14:
+            if valid_gate and pairs.get(RED, 0) > 12 or pairs.get(GREEN, 0) > 13 or pairs.get(BLUE, 0) > 14:
                 # print(pairs)
                 valid_gate = False
-                break
+                # break
         if valid_gate:
             count += game_id
 
-print(count)
+        games_power += max_cubes[BLUE] * max_cubes[GREEN] * max_cubes[RED]
+
+print(count, games_power)
