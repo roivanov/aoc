eea7fb329bb818cbc87f343326eb5df7f7e85e41
Roman Ivanov <me@roivanov.com>
2023-12-06 11:43:16 -0800

    day6 part2

--- 2023/day6/test_day6.py
+++ 2023/day6/test_day6.py
@@ -8,10 +8,13 @@ def hold(iter):
                 count+=1
         yield count
 
-def solve(fname):
+def solve(fname, kerning=False):
     with open(fname) as f:
         for line in f.readlines():
-            data = [int(x) for x in line.split(':')[1].strip().split()]
+            if not kerning:
+                data = [int(x) for x in line.split(':')[1].strip().split()]
+            else:
+                data = [int(''.join([x for x in line.split(':')[1].strip() if x.isdigit()]))]
             if line.startswith('Time'):
                 times = data
             elif line.startswith('Distance'):
@@ -25,5 +28,11 @@ def solve(fname):
 def test_part1_sample():
     assert solve('day6/data1part1sample.txt') == 288
 
+def test_part2_sample():
+    assert solve('day6/data1part1sample.txt', kerning=True) == 71503
+
 def test_part1_puzzle1():
     assert solve('day6/data1part1puzzle.txt') == 140220
+
+def test_part2_puzzle1():
+    assert solve('day6/data1part1puzzle.txt', kerning=True) == 140220
