329ac12418f18307a977a06a256f01f681e73831
Roman Ivanov <me@roivanov.com>
2023-12-08 11:07:55 -0800

    wip

--- 2023/day7/test_day7.py
+++ 2023/day7/test_day7.py
@@ -32,26 +32,40 @@ class Hand:
         self.s = s
         self.cards = [Card[x] if x.isalpha() else Card(int(x)) for x in s]
         self.__power = {k:len(list(v)) for k,v in groupby(sorted(self.cards))}
-    
+
     def power(self) -> HandPower:
-        # print(self.__power.values())
-        # print(max(self.__power.values()))
-        if max(self.__power.values()) == 5:
+        vals = sorted(self.__power.values())
+        if max(vals) == 5:
             return HandPower.FIVE
-        elif max(self.__power.values()) == 4:
+        elif max(vals) == 4:
             return HandPower.FOUR
-        elif set(self.__power.values()) == {3, 2}:
+        elif set(vals) == {3, 2}:
             return HandPower.FULLHOUSE
-        elif max(self.__power.values()) == 3:
+        elif max(vals) == 3:
             return HandPower.THREE
-        elif list(self.__power.values()) == [2, 2, 1]:
+        elif vals == [1, 2, 2]:
             return HandPower.TWO_PAIRS
-        elif max(self.__power.values()) == 2:
+        elif max(vals) == 2:
             return HandPower.PAIR
         return HandPower.NONE
 
+    def __str__(self) -> str:
+        return f"hand: {self.s}"
 
-def test_part1_sample():
+    def __repr__(self) -> str:
+        return f"hand: {self.s} {self.power()}"
+
+    def __lt__(self, other):
+        if self.power() != other.power():
+            return self.power() < other.power()
+        else:
+            for a, b in zip(self.s, other.s):
+                if a != b:
+                    return a < b
+
+        raise RuntimeError('must not be here')
+
+def test_part1_definitions():
     print(list(Card))
     print(Card(5))
     print(Card['A'])
@@ -65,5 +79,63 @@ def test_part1_sample():
     assert Hand('23332').power() == HandPower.FULLHOUSE
     assert Hand('TTT98').power() == HandPower.THREE
     assert Hand('23432').power() == HandPower.TWO_PAIRS
+    assert Hand('KK677').power() == HandPower.TWO_PAIRS
     assert Hand('A23A4').power() == HandPower.PAIR
     assert Hand('23456').power() == HandPower.NONE
+
+    assert Hand('33332') > Hand('2AAAA')
+
+
+EXAMPLE_DATA = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+"""
+
+def test_part1_sample():
+    hands = {}
+    for line in EXAMPLE_DATA.splitlines():
+        if line:
+            hand_str, bid = line.split(' ')
+            hand = Hand(hand_str)
+            hands[hand] = int(bid)
+
+    win = sum([hands[x] * (1+indx) for indx,x in enumerate(sorted(hands))])
+    assert win == 6440
+
+
+def test_part1_sample():
+    print(Hand('32T3J').power(), Hand('A9942').power())
+    assert Hand('32T3J') < Hand('A9942')
+
+    assert Hand('3JKKQ').power() == HandPower.PAIR
+
+    hands = {}
+    with open('day7/data1') as f:
+        for line in f.readlines()[:1006]:
+            # if line:
+            hand_str, bid = line.strip('\n').split(' ')
+            hand = Hand(hand_str)
+            hands[hand] = int(bid)
+
+    if len(hands) < 10:
+        print(', '.join([f"{x} {x.power().name} {hands[x]} * {(1+indx)}"
+                        for indx,x in enumerate(sorted(hands))]))
+    else:
+        assert hand_str == '38383'
+        assert bid == '612'
+
+
+    win = sum([hands[x] * (1+indx) for indx,x in enumerate(sorted(hands))])
+    prev = None
+    for each in sorted(hands):
+        if prev:
+            assert each > prev
+            prev = each
+
+    assert win < 252410386
+    assert win > 251498971
+    assert len(hands) == 1000
+    assert win == 1
